9e8d1a0c3adee1d11292aacc25be9687b7513a1e
Jos Kore Wingfield <jos@ellips.es>
2018-11-12 19:38:06 -0300

    travis shellcheck
    adding shellcheck installation to v_shellcheck to avoid version mismatches

--- .tests/v_shellcheck.sh
+++ .tests/v_shellcheck.sh
@@ -2,21 +2,19 @@
 set -euo pipefail
 
 v_shellcheck(){
+  export scversion="v0.4.6"
+  wget "https://storage.googleapis.com/shellcheck/shellcheck-${scversion}.linux.x86_64.tar.xz"
+  tar --xz -xvf shellcheck-"${scversion}".linux.x86_64.tar.xz
+  cp shellcheck-"${scversion}"/shellcheck /usr/bin/
+
   shellcheck --version \
     || err "Shellcheck not running correctly."
 
   cd "${BASEDIR}"
-  local ERRS
-  ERRS=$(find . -name '*.sh' -not \( -path "./.tests/*" -prune \) -print0 \
+  
+  find . -name '*.sh' -not \( -path "./.tests/*" -prune \) -print0 \
     | xargs -0 shellcheck -e SC1090 -e SC2016 -e SC2026 \
-    || true)
-
-  if [[ -n ${ERRS} ]]
-  then
-    find . -name '*.sh' -not \( -path "./.tests/*" -prune \) -print0 \
-      | xargs -0 shellcheck -e SC1090 -e SC2016 -e SC2026 -e SC1117
-    err "Shellcheck found errors."
-  fi
+    || err "Shellcheck found errors."
 
   success "Shellcheck validated with no errors!"
 }

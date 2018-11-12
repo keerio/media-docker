84a7cbf8c61626409b461f41a3e9c2e75a5385c2
Jos Kore Wingfield <jos@ellips.es>
2018-11-12 19:42:13 -0300

    Update .tests/v_shellcheck.sh
    shellcheck ci tweaking

--- .tests/v_shellcheck.sh
+++ .tests/v_shellcheck.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 v_shellcheck(){
-  export scversion="v0.4.6"
-  wget "https://storage.googleapis.com/shellcheck/shellcheck-${scversion}.linux.x86_64.tar.xz"
-  tar --xz -xvf shellcheck-"${scversion}".linux.x86_64.tar.xz
-  cp shellcheck-"${scversion}"/shellcheck /usr/bin/
+  export scversion="v0.5.0"
+  sudo wget "https://storage.googleapis.com/shellcheck/shellcheck-${scversion}.linux.x86_64.tar.xz"
+  sudo tar --xz -xvf shellcheck-"${scversion}".linux.x86_64.tar.xz
+  sudo cp shellcheck-"${scversion}"/shellcheck /usr/bin/
 
   shellcheck --version \
     || err "Shellcheck not running correctly."
@@ -13,7 +13,7 @@ v_shellcheck(){
   cd "${BASEDIR}"
   
   find . -name '*.sh' -not \( -path "./.tests/*" -prune \) -print0 \
-    | xargs -0 shellcheck -e SC1090 -e SC2016 -e SC2026 \
+    | xargs -0 shellcheck -e SC1090 -e SC2016 -e SC2026 -e SC1117\
     || err "Shellcheck found errors."
 
   success "Shellcheck validated with no errors!"

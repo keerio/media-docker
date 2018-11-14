de3b1232332b692e831d62dec85e5987b7bc3337
Jos Kore Wingfield <jos@ellips.es>
2018-11-14 19:29:37 -0300

    Update emerge_prereqs_install.sh
    correcting references to zypper in emerge prereq installer

--- .scripts/emerge_prereqs_install.sh
+++ .scripts/emerge_prereqs_install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-zypper_prereqs_install() {
-  sudo zypper remove -n docker docker-engine docker.io \
+emerge_prereqs_install() {
+  sudo emerge -C docker docker-engine docker.io \
     > /dev/null 2>&1 \
     || err "Error cleaning legacy packages."
 
-  run_sh "$SCRIPTDIR" "zypper_install" \
+  run_sh "$SCRIPTDIR" "emerge_install" \
     "curl" "git" "grep" "sed" "jq"
 }

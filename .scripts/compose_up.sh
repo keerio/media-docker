4271627a97304c8a6f08e91834dff52c1fbbfc7e
Jos Kore Wingfield <jos@ellips.es>
2018-11-14 20:47:07 -0300

    Update compose_up.sh
    add check for external docker network before trying to create it

--- .scripts/compose_up.sh
+++ .scripts/compose_up.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2143
 set -euo pipefail
 
 compose_up() {
-  sudo docker network create proxied \
-    > /dev/null 2>&1 || err "Error occured creating Docker network."
+  if [[ ! $(sudo docker network ls \
+    | grep proxied) ]] ; then
+      sudo docker network create proxied \
+        > /dev/null 2>&1 || err "Error occured creating Docker network."
+  fi
   sudo docker-compose up --force-recreate -d \
     > /dev/null 2>&1 || err "Error occured bringing up containers."
 }
